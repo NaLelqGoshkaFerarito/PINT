d01b6877300b56bdbf426f5c7841afd0693ef9c9
Stephen Wright <521476@student.saxion.nl>
2023-06-12 12:56:37 +0200

    finished ghome code

--- Google_Home_Code.ino
+++ Google_Home_Code.ino
@@ -1,63 +1,41 @@
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <FirebaseESP32.h>
-#include <ArduinoJson.h>
+#include <esp8266-google-home-notifier.h>
 
-#define FIREBASE_HOST "https://health-instruments--home-default-rtdb.europe-west1.firebasedatabase.app/"
-#define FIREBASE_AUTH "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"
-
-const char* ssid = "meh";
+const char* ssid     = "meh";
 const char* password = "askyourmom";
+const char* homeName = "Health Instruments";
+const char* message = "This message was brought to you by Health Instruments!";
 
-FirebaseData fbdo;
+GoogleHomeNotifier ghome;
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Hi");
-
+  
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(250);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.println("connected.");
 
-  Serial.println("Connected to WiFi");
-
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);
+  Serial.println("Finding Home...");
+  if (ghome.device(homeName,"en") != true) {
+    Serial.println(ghome.getLastError());
+    return;
+  }
+  Serial.print("Home found!(");
+  Serial.print(ghome.getIPAddress());
+  Serial.println(")");
+  
+  if (ghome.notify(message) != true) {
+    Serial.println(ghome.getLastError());
+    return;
+  }
 }
 
 void loop() {
-  if (Firebase.getBool(fbdo, "/assistant/talk")) {
-    String message = Firebase.getString(fbdo);
-    if (message.length() > 0) {
-      Serial.println("Google Assistant said: " + message);
-      sendBroadcast(message);
-    }
-    Firebase.setBool(fbdo, "/assistant/talk", false);
-  }
-
-  delay(1000);
-}
-
-void sendBroadcast(const String& message) {
-  DynamicJsonDocument json(256);
-  json["command"] = "com.google.cast.media.BroadcastCommand";
-  json["params"]["broadcastId"] = "RAMP";
-  json["params"]["payload"]["devices"] = "1123105FE0HNWV";
-  json["params"]["payload"]["message"] = message;
-
-  String body;
-  serializeJson(json, body);
-
-  WiFiClient client;
-  if (client.connect("localhost", 8008)) {
-    client.println("POST http://localhost:8008/setup/broadcast HTTP/1.1");
-    client.println("Content-Type: application/json");
-    client.println("Content-Length: " + String(body.length()));
-    client.println();
-    client.println(body);
-    client.println();
-    client.stop();
-  }
+  // other code here
 }
