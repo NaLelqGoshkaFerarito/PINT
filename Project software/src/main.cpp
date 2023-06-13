89c521e3be48fe531f4d57948845a197df6c4725
yourcomrade <MinhXecole@gmail.com>
2023-06-13 21:28:14 +0200

    Done with HTTP POST

--- Project software/src/main.cpp
+++ Project software/src/main.cpp
@@ -46,6 +46,40 @@ const unsigned char warning_icon [] PROGMEM = {
 	0x80, 0x00, 0x00, 0x04, 0x80, 0x00, 0x00, 0x04, 0xc0, 0x00, 0x00, 0x0c, 0xff, 0xff, 0xff, 0xfc, 
 	0xff, 0xff, 0xff, 0xfc, 0xff, 0xff, 0xff, 0xfc
 };
+
+const static char* root_mem_ca PROGMEM = \
+"-----BEGIN CERTIFICATE-----\n"
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
+"-----END CERTIFICATE-----\n";
+
 #define MAX_BRIGHTNESS 255
 
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
@@ -91,16 +125,16 @@ volatile msg my_own_msg;
 volatile bool warning ; //For patient health
 volatile bool wakeup ;
 struct alarm my_signal[2]={{0,13,1},{0,0,0}};
-//const char* ssid = "Ziggo5578958";
-//const char* password = "tgMsqtwd2spp";
-const char* ssid = "Nokia 3.4";
-const char* password = "Minh1234";
+const char* ssid = "Ziggo5578958";
+const char* password = "tgMsqtwd2spp";
+//const char* ssid = "Nokia 3.4";
+//const char* password = "Minh1234";
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
 const String id ="0001"; 
 const String server ="https://jessedijksma.com";
-const String apikey = "";
+const String apikey = "6fas6ABat7shfg14";
 // Interrupt for button to discard alert and warning
 void discard(void*arg){
   while(1){
@@ -321,16 +355,34 @@ String httpGETRequest(const char* serverName) {
 void HttpPostRequest(){
   if(my_own_msg.validHeartRate == 1 && my_own_msg.validSPO2 == 1){
     if(WiFi.status() == WL_CONNECTED){
-      WiFiClientSecure *client = new WiFiClientSecure;
-      client->setInsecure(); //don't use SSL certificate
+     WiFiClientSecure *client = new WiFiClientSecure;
+      client->setCACert(root_mem_ca);
       // Send the data using HTTPS
-       HTTPClient http;
+      HTTPClient http;
       String http_header = server + "/post-biometric-data.php";
       // Set the target URL
-      http.begin(http_header.c_str());
+      http.begin(*client,http_header.c_str());
 
       // Set the headers
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      String payload = "api_key=" + apikey +"&patientID=" + id \
+        + "&heartRate=" + String(my_own_msg.heartRate) + "&spO2=" + String(my_own_msg.spo2);
+      Serial.println(payload);
+      // Send HTTP POST request
+    int httpResponseCode = http.POST(payload);
+     
+    
+    
+    if (httpResponseCode>0) {
+      Serial.print("HTTP POST Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
 
     }
 
@@ -366,8 +418,8 @@ void signal(void*arg){
 
     } 
     
-    
-    vTaskDelay(pdMS_TO_TICKS(5000));
+    HttpPostRequest();
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
 void setup()
@@ -420,7 +472,7 @@ void setup()
   xTaskCreate(biometric_data_collect,"bio metric collector",4056,NULL,2,NULL);
   xTaskCreate(display_task,"display task",10045,NULL,2,NULL);
   xTaskCreate(signal,"Get signal",8048,NULL,2,NULL);
-  xTaskCreate(discard,"Discard signal",2000,NULL,3,NULL);
+  xTaskCreate(discard,"Discard signal",1000,NULL,3,NULL);
   vTaskStartScheduler();
 }
 
